1138b25fc53dac3de99e7df4e446b073944b3015
abc <2001abhaybhardwaj@gmail.com>
2023-03-12 12:17:43 +0530

    Basic printing in Python

---
+++ Basics of Python/printing.py
@@ -0,0 +1,14 @@
+# printing in python
+
+print("Hello")  # Hello
+print(1+3)  # 4
+print(10*3)  # 30
+print(3.5*10)  # return ans in floating points : 35.0
+print()  # gives extra space
+print("Name : Abhay")  # print name
+
+# it will print this as |  abhay-anirudh-shobhit |
+print("abhay", "anirudh", "shobhit", sep="-")
+
+# it will print this as |  abhay anirudh shobhit@ |
+print("abhay", "anirudh", "shobhit", end="@")
