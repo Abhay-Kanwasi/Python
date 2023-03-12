58443a089472387605c3026077bdd01f26f41ea2
abc <2001abhaybhardwaj@gmail.com>
2023-03-12 15:24:55 +0530

    escape seq in python

---
+++ Basics of Python/escapeSeq.py
@@ -0,0 +1,21 @@
+# escape sequence or charcaters in python
+
+print("I am \nabhay bhardwaj")  # skips to next line
+
+print("1.hello\t2.my\t3.name\t4.is\t5.abhay")  # gives spacing in string
+
+print('It\'s, python language')  # \ is used to escape charcter in programming
+
+print("""This is a "great" course,""")
+
+# raw string
+
+# r : used to declear raw string
+print(r"https:\\www.google.com\temp\new_course.cpp")
+
+# \b shift string one step back
+print("abhay\bbhardwaj")
+
+# \r : carriage return
+
+print("abhay bhardwaj \rhello")
