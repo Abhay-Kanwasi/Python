f27eae7550d5cc16989b64ebc787da58a90e1045
abc <2001abhaybhardwaj@gmail.com>
2023-03-13 23:48:27 +0530

    string operators

---
+++ Basics of Python/stringOp.py
@@ -0,0 +1,21 @@
+
+str = "abhay"
+
+# printing characters in loop
+for i in str:
+    print(i)
+
+str2 = "bhardwaj"
+
+# concet..
+print(str+str2)
+
+# operator with string
+print(str * 3)  # printing strars three times
+
+# check substr in string
+
+str3 = "hello"
+
+print("he" in str3)  # true
+print("hem" in str3)  # false
